8e610c53757999259d86c429bb2f20c204f9e852
Venkat.R <ramsunvtech@gmail.com>
2023-01-21 14:51:34 +0800

    feat(home): update home page panes

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,21 +1,62 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  int _selectedIndex = 0;
+  // int _selectedIndex = 0;
   // int currentScore = 50;
   // int targetScore = 90;
 
-  @override
+  // @override
   void _onItemTapped(int index) {}
+
+  List<Widget> homePanes = [
+    const Center(
+      child: Image(
+        image: AssetImage('assets/cute.webp'),
+      ),
+    ),
+    const Center(
+      child: Text(
+        'Current Score: 50%',
+        style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 25,
+            fontStyle: FontStyle.italic),
+      ),
+    ),
+    const Center(
+      child: Text(
+        'Target Score: 90%',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 34,
+          fontStyle: FontStyle.italic,
+          color: Colors.blue,
+        ),
+      ),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Pingy')),
-      body: const Center(
-          child: Image(
-            image: AssetImage('assets/cute.webp'),
-          ),
+      body: ListView.builder(
+        itemCount: homePanes.length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              print("tapped");
+            },
+            child: new Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: new Container(
+                child: homePanes[index],
+              ),
+            ),
+          );
+        },
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const [
@@ -32,7 +73,7 @@ class HomeScreen extends StatelessWidget {
             label: 'Task',
           ),
         ],
-        currentIndex: _selectedIndex,
+        // currentIndex: _selectedIndex,
         onTap: _onItemTapped,
       ),
     );
