eb1b35b3173158ded3b5958036c59a18b106d05d
Venkat.R <ramsunvtech@gmail.com>
2023-01-21 11:39:55 +0800

    tech(common): fix warnings and errors

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+// Screen Imports.
+import 'screens/home.dart';
+
 void main() {
   runApp(
     PingyApp(),
@@ -10,27 +13,8 @@ class PingyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Pingy App')),
-        body: const Center(
-            child: Image(
-          image: AssetImage('assets/cute.webp'),
-        )),
-        bottomNavigationBar: const BottomNavigationBar(items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add),
-            label: 'Task Types',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add),
-            label: 'Rewards',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add),
-            label: 'Task',
-          ),
-        ]),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen()
     );
   }
 }
