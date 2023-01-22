187222dae8bd286956f8c150aa1cacafdf99aa87
Venkat.R <ramsunvtech@gmail.com>
2023-01-22 19:26:14 +0800

    feat(main): clean task type screen

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class PingyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen()
+      home: HomeScreen(),
     );
   }
 }
