1e4370f09e17df0e05f8c3810d9fbbd468a61ae8
Venkat.R <ramsunvtech@gmail.com>
2023-01-22 22:13:03 +0800

    feat(update-task): update list tile list of task types update to state ful widget

--- lib/screens/task/update_task.dart
+++ lib/screens/task/update_task.dart
@@ -1,14 +1,117 @@
 import 'package:flutter/material.dart';
+import 'package:pingy/models/TaskType.dart';
 
-class UpdateTaskScreen extends StatelessWidget {
+class UpdateTaskScreen extends StatefulWidget {
+  @override
+  _UpdateTaskScreenState createState() => _UpdateTaskScreenState();
+}
+
+class _UpdateTaskScreenState extends State<UpdateTaskScreen> {
+  List<TaskTypeModel> taskTypes = [
+    TaskTypeModel('Breakfast', 250, false),
+    TaskTypeModel('Lunch', 150, false),
+    TaskTypeModel('Dinner', 200, false),
+  ];
+
+  TaskTypeModel selectedTaskType = TaskTypeModel('default', 100, false);
+
+  final List<Widget> homePanes = [
+    const Center(
+      child: Image(
+        image: AssetImage('assets/cute.webp'),
+      ),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Pingy (Update Task)')),
-      body: const Center(
-        child: Text('Task Type Form'),
+      body: SafeArea(
+          child: Column(
+        children: [
+          Expanded(
+              child: ListView.builder(
+                itemCount: taskTypes.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return taskItem(
+                    taskTypes[index].taskName,
+                    taskTypes[index].mark,
+                    taskTypes[index].isSelected,
+                    index,
+                  );
+                },
+          )),
+          TextField(
+            onChanged: (value) {
+              //Do something with the user input.
+            },
+            decoration: InputDecoration(
+              hintText: 'Enter Score',
+              contentPadding:
+                  EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(32.0)),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderSide:
+                    BorderSide(color: Colors.lightBlueAccent, width: 1.0),
+                borderRadius: BorderRadius.all(Radius.circular(32.0)),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide:
+                    BorderSide(color: Colors.lightBlueAccent, width: 2.0),
+                borderRadius: BorderRadius.all(Radius.circular(32.0)),
+              ),
+            ),
+          ),
+          Center(
+            child: Text(
+              'Today Marks: ${selectedTaskType.taskName}',
+              style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25,
+                  fontStyle: FontStyle.italic),
+            ),
+          ),
+        ],
+      )),
+    );
+  }
+
+  Widget taskItem(String taskName, int mark, bool isSelected, int index) {
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundColor: Colors.green[700],
+        child: const Icon(
+          Icons.person_outline_outlined,
+          color: Colors.white,
+        ),
       ),
+      title: Text(
+        taskName,
+        style: const TextStyle(
+          fontWeight: FontWeight.w500,
+        ),
+      ),
+      subtitle: const Text('Task Type description'),
+      trailing: isSelected
+          ? const Icon(
+              Icons.check_circle,
+              color: Colors.green,
+            )
+          : const Icon(
+              Icons.check_circle_outline,
+              color: Colors.grey,
+            ),
+      onTap: () {
+        setState(() {
+          taskTypes[index].isSelected = !taskTypes[index].isSelected;
+          if (taskTypes[index].isSelected == true) {
+            selectedTaskType = TaskTypeModel(taskName, 160, true);
+          }
+        });
+      },
     );
   }
 }
