e85e3c141803e774e9205523674a0ddcb99cb77d
Venkat.R <ramsunvtech@gmail.com>
2023-01-22 21:11:37 +0800

    feat(update-task): add new screen

---
+++ lib/screens/update_task.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class UpdateTaskScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Pingy (Update Task)')),
+      body: const Center(
+        child: Text('Task Type Form'),
+      ),
+    );
+  }
+}
