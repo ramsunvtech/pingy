f431559f40fc9af6d83c8e47190f951da04768f4
Venkat.R <ramsunvtech@gmail.com>
2023-01-25 22:37:58 +0800

    tech(model/hive): update activity type with hive

--- lib/models/activity_type.dart
+++ lib/models/activity_type.dart
@@ -1,6 +1,12 @@
-class ActivityTypeModel {
-  String activityName;
-  int mark;
+import 'package:hive/hive.dart';
+
+@HiveType(typeId: 0)
+class ActivityTypeModel extends HiveObject {
+  @HiveField(0)
+  final String activityName;
+
+  @HiveField(1)
+  final String mark;
 
   ActivityTypeModel(this.activityName, this.mark);
 }
