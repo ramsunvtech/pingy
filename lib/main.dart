0bc0d46f4941f52aac25ce249eef5cdfa5c36890
Venkat.R <ramsunvtech@gmail.com>
2023-01-26 12:11:21 +0800

    tech(main): add activity type adapters

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:pingy/models/activity.dart';
+import 'package:pingy/models/activity_type.dart';
 
 // Screen Imports.
 import 'screens/home.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  final appDocDir = await getApplicationDocumentsDirectory();
 
   // Initialize hive.
   await Hive.initFlutter();
 
-  // Register.
-  Hive.registerAdapter(ActivityAdapter());
+  Hive
+    ..init(appDocDir.path)
+    ..registerAdapter(ActivityAdapter())
+    ..registerAdapter(ActivityTypeModelAdapter());
 
   // Open Activity, Activity Type Box.
   await Hive.openBox('activity');
