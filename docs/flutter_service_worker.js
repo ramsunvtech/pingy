b16a752e9b37e92713a7f660bbf1f24f33619b41
Venkat.R <ramsunvtech@gmail.com>
2023-01-28 18:43:21 +0800

    tech(web): update release manifest

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "73f9d6e9db7f32f3e125cefc801f3371",
-"index.html": "3393c18d8e3cf9156ccc950945213abd",
-"/": "3393c18d8e3cf9156ccc950945213abd",
-"main.dart.js": "d650fe7a34afed23b4e2b14542788039",
+"index.html": "fc86d3f11adb946373e83900dbacb534",
+"/": "fc86d3f11adb946373e83900dbacb534",
+"main.dart.js": "84c0182f2a62396117f81659425b0d34",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
