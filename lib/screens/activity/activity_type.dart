0f5a3029578646adceb8d952461aec242feccc4d
Venkat.R <ramsunvtech@gmail.com>
2023-02-25 19:54:05 +0800

    feat(activity-type): update simple go back navigation

--- lib/screens/activity/activity_type.dart
+++ lib/screens/activity/activity_type.dart
@@ -85,12 +85,7 @@ class _TaskTypeScreenState extends State<TaskTypeScreen> {
                 ActivityTypeModel newActivityType = ActivityTypeModel(activityTypeId, _nameController.text, _fullScoreController.text);
                 activityTypeBox.put(activityTypeId, newActivityType);
 
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (builder) => ActivityTypeListScreen(),
-                  ),
-                );
+                Navigator.of(context).pop();
               },
               // padding: const EdgeInsets.only(left: 30, right: 30, top: 15, bottom: 15),
               // color: Colors.pink,
