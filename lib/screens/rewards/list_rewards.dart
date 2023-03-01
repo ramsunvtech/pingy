de66166e8ba6f691cda92c37c6d1b191edbe595d
Venkat.R <ramsunvtech@gmail.com>
2023-03-01 19:18:38 +0800

    style(rewards): add floating button color

--- lib/screens/rewards/list_rewards.dart
+++ lib/screens/rewards/list_rewards.dart
@@ -34,7 +34,7 @@ class _RewardsListScreenState extends State<RewardsListScreen> {
         );
       },
       child: const Icon(Icons.add),
-      backgroundColor: Colors.green,
+      backgroundColor: const Color(0xFF98006D),
     );
   }
 
