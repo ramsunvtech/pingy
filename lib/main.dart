6ca9ea3635600a340ba2dce975ef60eef6a1a101
Venkat.R <ramsunvtech@gmail.com>
2023-03-05 12:36:05 +0800

    tech(main): remove activity creation in main script

--- lib/main.dart
+++ lib/main.dart
@@ -31,40 +31,6 @@ void main() async {
     ..registerAdapter(ActivityItemAdapter())
     ..registerAdapter(RewardsModelAdapter());
 
-  // Open Activity Type, Rewards and Activity Box.
-  final activityTypeBox = await Hive.openBox('activity_type');
-  var rewardBox = await Hive.openBox('rewards');
-  var activityBox = await Hive.openBox('activity');
-
-  // Add Today Activity if not exist.
-  var today = DateTime.now();
-  var activityId = 'activity_${today.year}${today.month}${today.day}';
-  bool canCreateNewActivity = true;
-
-  if (rewardBox.isEmpty || activityTypeBox.isEmpty) {
-    canCreateNewActivity = false;
-  }
-
-  if (activityBox.containsKey(activityId)) {
-    // print('Log: Today Activity is exist');
-    Activity todayActivity = activityBox.get(activityId);
-    if (todayActivity.activityItems.isNotEmpty) {
-      // print('Log: Today Activity Type is not empty');
-      canCreateNewActivity = false;
-    }
-  }
-
-  if (canCreateNewActivity) {
-    final activityTypeKeys = await activityTypeBox.keys;
-    final List<ActivityItem> activityItems = [];
-    for (var activityTypeKey in activityTypeKeys) {
-      ActivityItem newActivityItem = ActivityItem(activityTypeKey, '');
-      activityItems.add(newActivityItem);
-    }
-    Activity newActivity = Activity(activityId, activityItems, '', DateTime.now());
-    activityBox.put(activityId, newActivity);
-  }
-
   runApp(
     PingyApp(),
   );
