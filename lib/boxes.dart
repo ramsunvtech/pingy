f9d7bc34b148bb3a7d85cd51b9d739b8cf2450dc
Venkat.R <ramsunvtech@gmail.com>
2023-03-11 23:59:06 +0800

    tech(boxes): remove unused boxes file

--- lib/boxes.dart
+++
@@ -1,7 +0,0 @@
-import 'package:hive/hive.dart';
-import 'package:pingy/models/hive/activity.dart';
-
-class Boxes {
-  static Box<Activity> getActivities() =>
-      Hive.box<Activity>('activity');
-}
