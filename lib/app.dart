7c7e6a24ba9329f337a78906a5dd6981457915b6
Venkat.R <ramsunvtech@gmail.com>
2023-03-13 21:36:24 +0800

    tech(app): update color

--- lib/app.dart
+++ lib/app.dart
@@ -5,6 +5,7 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import 'package:pingy/screens/home.dart';
+import 'package:pingy/utils/color.dart';
 
 class PingyApp extends StatefulWidget {
   @override
@@ -27,7 +28,7 @@ class PingyAppState extends State<PingyApp> {
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(colorSchemeSeed: const Color(0xff6750a4)),
+      theme: ThemeData(colorSchemeSeed: purpleColor),
       home: HomeScreen(),
       localizationsDelegates: const [
         AppLocalizations.delegate,
