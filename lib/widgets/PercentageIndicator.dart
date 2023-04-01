825e29624878dba09d556527134c416786335fdc
Venkat.R <ramsunvtech@gmail.com>
2023-04-02 07:24:41 +0800

    tech(widget): update animation duration

--- lib/widgets/PercentageIndicator.dart
+++ lib/widgets/PercentageIndicator.dart
@@ -12,6 +12,7 @@ dynamic percentageIndicator(double radius, String score, String label) {
       radius: radius,
       lineWidth: 13.0,
       animation: true,
+      animationDuration: 700,
       percent: percentValue,
       center: Text(
         '$score%',
