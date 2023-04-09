88536fe35a4844c6989080698b99dfd7c6898611
Venkat.R <ramsunvtech@gmail.com>
2023-04-09 22:45:58 +0800

    fix(ios): add web check

--- lib/utils/permissions.dart
+++ lib/utils/permissions.dart
@@ -1,16 +1,23 @@
 import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 Future askCameraPermission() async {
-  if (await Permission.camera.request().isGranted) {}
+  if (!kIsWeb) {
+    if (await Permission.camera.request().isGranted) {}
+  }
 }
 
 /// Checks the notification permission status
 Future<String> getCheckNotificationPermStatus() async {
-  if (await Permission.notification.request().isGranted) {
-    String notificationPermissionStatus =
-    Permission.notification.status as String;
+  if (!kIsWeb) {
+    if (await Permission.notification.request().isGranted) {
+      String notificationPermissionStatus =
+      Permission.notification.status as String;
 
-    return notificationPermissionStatus;
+      return notificationPermissionStatus;
+    }
+
+    return '';
   }
 
   return '';
