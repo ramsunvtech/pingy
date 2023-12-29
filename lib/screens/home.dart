b0a3f5bf1fadf8a705003c27231c85fb178188a4
Venkat.R <ramsunvtech@gmail.com>
2023-12-29 22:16:55 +0800

    tech(android/release-debug): update debug apk

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -99,7 +99,7 @@ class _HomeScreenState extends State<HomeScreen> with WidgetsBindingObserver {
                   color: Colors.grey.withOpacity(0.3), // Soft grey shadow
                   spreadRadius: 1, // Extend the shadow to all sides by 1 pixel
                   blurRadius: 5, // Soften the shadow by blurring it
-                  offset: Offset(0, 3), // Position the shadow below the avatar
+                  offset: const Offset(0, 3), // Position the shadow below the avatar
                 ),
               ],
             ),
