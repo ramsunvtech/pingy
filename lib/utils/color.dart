81a08a2fd25c5fc6ed21864bd5c390160cc8e46e
Venkat.R <ramsunvtech@gmail.com>
2024-01-03 23:13:24 +0800

    color/utils: add black and light black

--- lib/utils/color.dart
+++ lib/utils/color.dart
@@ -17,3 +17,7 @@ Color rewardColor = const Color(0xff32864E);
 Color lightGreenColor = const Color(0xffD7EADD);
 
 const Color iconColor = Color(0xff0A150E);
+
+Color blackColor = const Color(0xFF000000);
+
+Color lightBlackColor = const Color(0xFF202020);
