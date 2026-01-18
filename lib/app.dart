a71fb0d8109ea429bc744dc107ba2b95e503fd28
Venkat.R <ramsunvtech@gmail.com>
2026-01-18 13:04:15 +0800

    feat:

--- lib/app.dart
+++ lib/app.dart
@@ -10,17 +10,41 @@ import 'package:pingy/screens/home.dart';
 // Utils.
 import 'package:pingy/utils/color.dart';
 
+// Services.
+import 'package:pingy/services/notification.dart';
+
 class PingyApp extends StatefulWidget {
   @override
   PingyAppState createState() => PingyAppState();
 }
 
-class PingyAppState extends State<PingyApp> {
+class PingyAppState extends State<PingyApp> with WidgetsBindingObserver {
+  
+  @override
+  void initState() {
+    super.initState();
+    // Add lifecycle observer
+    WidgetsBinding.instance.addObserver(this);
+  }
+
   @override
   void dispose() {
+    // Remove lifecycle observer
+    WidgetsBinding.instance.removeObserver(this);
     // Closes all Hive boxes
     Hive.close();
     super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+    
+    if (state == AppLifecycleState.resumed) {
+      // App came to foreground - reschedule notifications
+      print('📱 App resumed - checking notifications...');
+      NotificationService.rescheduleAll();
+    }
   }
 
   @override
@@ -31,7 +55,7 @@ class PingyAppState extends State<PingyApp> {
       theme: ThemeData(
         colorSchemeSeed: purpleColor,
         scaffoldBackgroundColor: whiteColor,
-        brightness: Brightness.light, // Add this
+        brightness: Brightness.light,
         textTheme: const TextTheme(
           bodyLarge: TextStyle(color: Colors.black87),
           bodyMedium: TextStyle(color: Colors.black87),
@@ -39,7 +63,7 @@ class PingyAppState extends State<PingyApp> {
         ),
       ),
       darkTheme: ThemeData(
-        brightness: Brightness.light, // 🔴 FORCE LIGHT EVEN IN DARK
+        brightness: Brightness.light, // FORCE LIGHT EVEN IN DARK
       ),
       home: HomeScreen(),
       localizationsDelegates: const [
