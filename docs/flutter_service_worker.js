1c8f087856bbbb078806cf79958fad3629ec9a43
Venkat.R <ramsunvtech@gmail.com>
2026-01-20 09:24:34 +0800

    tech(web/release): update web assets & build

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "0beacca1459ceb4873d52672c9e67dca",
+const RESOURCES = {"flutter_bootstrap.js": "06fe9ab33abd448f0f6c81507c80cc0e",
 "version.json": "73f9d6e9db7f32f3e125cefc801f3371",
 "splash/img/light-2x.png": "07a4934243ced46c03cccff4663bdd2a",
 "splash/img/dark-4x.png": "82aad15811c14accb060abf576ccc1cd",
@@ -15,10 +15,10 @@ const RESOURCES = {"flutter_bootstrap.js": "0beacca1459ceb4873d52672c9e67dca",
 "splash/img/light-1x.png": "a7edf1a6a33bda51504a003ba9472abb",
 "splash/splash.js": "c6a271349a0cd249bdb6d3c4d12f5dcf",
 "splash/style.css": "21da7b439fd18f87dced06bb1f62610b",
-"index.html": "92ec90ae1b9c54647ffad23958f705e1",
-"/": "92ec90ae1b9c54647ffad23958f705e1",
+"index.html": "a28034ee36587a5d390a730d733f5c48",
+"/": "a28034ee36587a5d390a730d733f5c48",
 "privacy-policy.html": "56e2df20397d021fd932d4a98d017474",
-"main.dart.js": "762e25a826e939483b92a5427cfdb515",
+"main.dart.js": "5deaa97873836e88785495a1006e5435",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
