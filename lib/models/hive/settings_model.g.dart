4be9bde446c5fc0148c6712791ce670e8858bcf2
Venkat.R <ramsunvtech@gmail.com>
2026-02-02 21:47:33 +0800

    feat:

---
+++ lib/models/hive/settings_model.g.dart
@@ -0,0 +1,44 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'settings_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class SettingsModelAdapter extends TypeAdapter<SettingsModel> {
+  @override
+  final int typeId = 10;
+
+  @override
+  SettingsModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return SettingsModel(
+      settingKey: fields[0] as String,
+      value: fields[1] as Object?,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, SettingsModel obj) {
+    writer
+      ..writeByte(2)
+      ..writeByte(0)
+      ..write(obj.settingKey)
+      ..writeByte(1)
+      ..write(obj.value);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is SettingsModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
