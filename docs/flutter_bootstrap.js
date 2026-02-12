8d5ca2e90f648341bfd8a9e1c1650c168bb9283c
Venkat.R <ramsunvtech@gmail.com>
2026-02-12 21:54:58 +0800

    tech(web/release): update web assets & build

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1609545657"
+    serviceWorkerVersion: "1925016770"
   }
 });
